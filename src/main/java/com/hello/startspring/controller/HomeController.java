bf29e938941c53ba307f10aec179101d1e7fbb42
Woojin Shin <gintooooonic@gmail.com>
2022-01-21 18:22:27 +0900

    "회원 관리 예제 - 웹 MVC 개발" 완료

---
+++ src/main/java/com/hello/startspring/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.hello.startspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+  @GetMapping("/")
+  public String home() {
+    return "home";
+  }
+}
